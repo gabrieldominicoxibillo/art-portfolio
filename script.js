fea1712649df9d1527bca5392bf2bb288f4235ca
gabrieldominicoxibillo <115641762+gabrieldominicoxibillo@users.noreply.github.com>
2024-02-13 06:25:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    // Handle navbar visibility
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+
+    // Handle scroll progress bar
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
